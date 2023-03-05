5a76ddcff77b1138636891613cd661a531f5459d
Kalips2 <100539578+Kalips2@users.noreply.github.com>
2023-03-05 20:02:54 +0200

    Second commit

--- Constants.h
+++ Constants.h
@@ -5,10 +5,10 @@
 #define STUDIO_GARBAGE_MAX_SIZE 4000
 #define PARTITION_GARBAGE_MAX_SIZE 4000
 
-#define STUDIO_INDEX "D:\\newLaba\\untitled3\\Information\\Studio.ind"
-#define STUDIO_DATA "D:\\newLaba\\untitled3\\Information\\Studio.fl"
-#define PARTITION_DATA "D:\\newLaba\\untitled3\\Information\\Partitions.fl"
-#define PARTITION_GARBAGE "D:\\newLaba\\untitled3\\Information\\PartitionsGarbage.fl"
-#define STUDIO_GARBAGE "D:\\newLaba\\untitled3\\Information\\StudioGarbage.fl"
+#define STUDIO_INDEX "D:\\LabaBdV2\\untitled3\\Information\\Studio.ind"
+#define STUDIO_DATA "D:\\LabaBdV2\\untitled3\\Information\\Studio.fl"
+#define PARTITION_DATA "D:\\LabaBdV2\\untitled3\\Information\\Partitions.fl"
+#define PARTITION_GARBAGE "D:\\LabaBdV2\\untitled3\\Information\\PartitionsGarbage.fl"
+#define STUDIO_GARBAGE "D:\\LabaBdV2\\untitled3\\Information\\StudioGarbage.fl"
 
 #endif //UNTITLED3_CONSTANTS_H
